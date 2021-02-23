9f4950542f56124cf5ef426cfe20d52a5413e92a
Ruben Matamoros <ruben@valiantcreative.net>
2021-02-23 15:43:30 -0500

    added favicon path

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(dirname, "build")));
 
 app.use(require('./controllers/'));
 
